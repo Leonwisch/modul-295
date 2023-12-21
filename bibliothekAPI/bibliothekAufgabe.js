ec58805948d1efaa255a5de818beda840e25a2e9
Leonwisch <leon.faessler@yahoo.com>
2023-12-21 15:44:31 +0000

    ende tag 4

--- bibliothekAPI/bibliothekAufgabe.js
+++ bibliothekAPI/bibliothekAufgabe.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const app = express();
 const port = 3000;
+const session = require('express-session')
+
+app.use('/swagger-ui', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use(session({
+  secret: 'supersecret',
+	resave: false,
+	saveUninitialized: false,
+  cookie: {}
+}))
 
 // Hier lade ich die Express JSON Middleware, damit ich an meine Endpunkte JSON-Daten im Body senden kann und diese direkt als JavaScript Objekt verfügbar werden.
 app.use(express.json());
+
+const secretAdminCredentials = { email: "desk@library.example", password: "m295" }
 
 // generated with ChatGPT
 let books = [
@@ -16,51 +30,76 @@ let books = [
   { isbn: "978-0679735779", title: "Beloved", year: "1987", author: "Toni Morrison" },
   { isbn: "978-0316769488", title: "The Catcher in the Rye", year: "1951", author: "J.D. Salinger" },
   { isbn: "978-0143039433", title: "Never Let Me Go", year: "2005", author: "Kazuo Ishiguro" },
-  { isbn: "978-0345804310", title: "Gone Girl", year: "2012", author: "Gillian Flynn" }
+  { isbn: "978-0345804310", title: "Gone Girl", year: "2012", author: "Gillian Flynn" } 
 ];
 
-// generated with ChatGPT
 let lends = [
-  { id: "092134877773", customer_id: "1", isbn: "978-0143124177", borrowed_at: "13.12.2023", returned_at: ""},
-  { id: "092134877774", customer_id: "2", isbn: "978-0307277671", borrowed_at: "14.12.2023", returned_at: ""},
-  { id: "092134877775", customer_id: "3", isbn: "978-0553386790", borrowed_at: "15.12.2023", returned_at: ""},
-  { id: "092134877776", customer_id: "4", isbn: "978-0812995343", borrowed_at: "16.12.2023", returned_at: ""},
-  { id: "092134877777", customer_id: "5", isbn: "978-0375831003", borrowed_at: "17.12.2023", returned_at: ""},
-  { id: "092134877778", customer_id: "6", isbn: "978-1501132957", borrowed_at: "18.12.2023", returned_at: ""},
-  { id: "092134877779", customer_id: "7", isbn: "978-0679735779", borrowed_at: "19.12.2023", returned_at: ""},
-  { id: "092134877780", customer_id: "8", isbn: "978-0316769488", borrowed_at: "20.12.2023", returned_at: ""},
-  { id: "092134877781", customer_id: "9", isbn: "978-0143039433", borrowed_at: "21.12.2023", returned_at: ""},
-  { id: "092134877782", customer_id: "10", isbn: "978-0345804310", borrowed_at: "22.12.2023", returned_at: ""}
+  { id: "1", customer_id: "12", isbn: "1234567890", borrowed_at: "2023-01-01", returned_at: "2023-01-15" },
+  { id: "2", customer_id: "18", isbn: "0987654321", borrowed_at: "2023-02-10", returned_at: "2023-03-05" },
+  { id: "3", customer_id: "25", isbn: "2345678901", borrowed_at: "2023-04-20", returned_at: "2023-05-15" },
+  { id: "4", customer_id: "31", isbn: "3456789012", borrowed_at: "2023-06-15", returned_at: "2023-07-01" },
+  { id: "5", customer_id: "42", isbn: "4567890123", borrowed_at: "2023-08-05", returned_at: "2023-08-25" },
+  { id: "6", customer_id: "13", isbn: "5678901234", borrowed_at: "2023-09-10", returned_at: "2023-10-01" },
+  { id: "7", customer_id: "67", isbn: "6789012345", borrowed_at: "2023-11-15", returned_at: "2023-12-05" },
+  { id: "8", customer_id: "78", isbn: "7890123456", borrowed_at: "2024-01-01", returned_at: "2024-01-15" },
+  { id: "9", customer_id: "89", isbn: "8901234567", borrowed_at: "2024-02-10", returned_at: "2024-03-05" },
+  { id: "10", customer_id: "94", isbn: "9012345678", borrowed_at: "2024-04-20", returned_at: "2024-05-15" }
 ];
 
+//Login
+app.post('/login', function (request, response) {
+	const { email, password } = request.body
 
+	// Check the credentials against store
+	if (email?.toLowerCase() == secretAdminCredentials.email && password == secretAdminCredentials.password) {
+
+		// Link email to session
+		request.session.email = email
+
+		return response.status(200).json({ email: request.session.email })
+	}
+
+  return response.status(401).json({ error: "Invalid credentials" })
+})
+
+app.get('/verify', function (request, response) {
+	
+	// Check if email is set in session
+	if (request.session.email) {
+		return response.status(200).json({ email: request.session.email })
+	}
+
+  return response.status(401).json({ error: "Not logged in" })
+})
+
+app.delete('/logout', function (request, response) {
+
+	// Check if email is set in session
+	if (request.session.email) {
+
+		// Reset link of session to email
+		request.session.email = null
+
+		return response.status(204).send()
+	}
+
+  return response.status(401).json({ error: "Not logged in" })
+})
+
+// Books
 app.get('/books', (request, response) => {
   response.send(books);
-});
-
-app.get('/lends', (request, response) => {
-  response.send(lends);
-});
-
-app.get('/lends/:id', (request, response) => {
-  response.send(lends.find((lend) => lend.id === request.params.id ))
 });
 
 app.get('/books/:isbn', (request, response) => {
   response.send(books.find((book) => book.isbn === request.params.isbn ))
 });
 
-app.post('/lends', (request, response) => {
-  lends.push(request.body);
-  response.status(201).send(lends);
-});
-
-
 app.post('/books', (request, response) => {
   // immutable manipulation
-  //books = [...books, request.body];
+  books = [...books, request.body];
   // mutable manipulation
-  books.push(request.body);
+  // books.push(request.body);
   response.status(201).send(books);
 });
 
@@ -78,14 +117,6 @@ app.put('/books/:isbn', (request, response) => {
   response.send(books);
 });
 
-app.patch('/lends/:id', (request, response) => {
-  const keys = Object.keys(request.body);
-  const oldlend = lends.find((lend) => lend.id === request.params.id );
-  keys.forEach((key) => oldlend[key] = request.body[key]);
-  lends = lends.map((lend) => lend.id === request.params.id ? oldlend : lend);
-  response.send(lends);
-});
-
 app.patch('/books/:isbn', (request, response) => {
   const keys = Object.keys(request.body);
   const oldBook = books.find((book) => book.isbn === request.params.isbn );
@@ -99,6 +130,41 @@ app.delete('/books/:isbn', (request, response) => {
   response.send(books);
 });
 
+// Lends
+app.get('/lends', (request, response) => {
+  if(request.session.email){
+    return response.send(lends);
+  }
+  return response.status(401).json({ error: "Not logged in" })
+});
+
+app.get('/lends/:id', (request, response) => {
+  if(request.session.email){
+  return response.send(lends.find((lend) => lend.id === request.params.id))
+}
+return response.status(401).json({ error: "Not logged in" })
+});
+
+app.post('/lends', (request, response) => {
+  if(request.session.email){
+  const newLend = request.body;
+  newLend['borrowed_at'] = new Date().toISOString();
+  lends = [...lends, request.body];
+  return response.status(201).send(lends);
+}
+return response.status(401).json({ error: "Not logged in" })
+});
+
+app.delete('/lends/:id', (request, response) => {
+  if(request.session.email){
+  const returnedLend = lends.find((lend) => lend.id === request.params.id)
+  returnedLend['returned_at'] = new Date().toISOString();
+  return response.send(lends);
+}
+return response.status(401).json({ error: "Not logged in" })
+});
+
+// Server
 app.listen(port, () => {
   console.log(`Bookstore app listening on port ${port}`);
 });
